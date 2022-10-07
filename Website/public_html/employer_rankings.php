0bf796d1c6af4874cedce391911524eacf9a6c19
Sebastian Sosa <sebastiansosa1@gmail.com>
2022-10-07 16:50:40 +1300

    Models for each page added.

--- Website/public_html/employer_rankings.php
+++ Website/public_html/employer_rankings.php
@@ -2,10 +2,22 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <title>Employer Rankings</title>
+    <title>FakeName.com - Overview</title>
 </head>
 <body>
+<nav>
+    <ul>
+        <li><a href="index.html">Home</a></li>
+        <li><a href="employer_rankings.php">Employer Rankings</a></li>
+        <li><a href="review_employer.php">Review an Employer</a></li>
+        <li>Sign In / Register</li>
+    </ul>
+</nav>
 <h1>Employer Ratings</h1>
+
+<p>Overview Page - Add search bar to browse employers</p>
+
+<p>Show top employers and ratings as links to view review...</p>
 <?php
 try {
 //    $open_review_s_db = new PDO("sqlite:open_review_s_sqlite.db");
@@ -16,7 +28,7 @@ try {
 }
 
 try {
-    $res = $open_review_s_db->query("SELECT company_name, overall_rating FROM reviewedEmployer_S WHERE employer_id = 318448");
+    $res = $open_review_s_db->query("SELECT company_name, overall_rating FROM reviewedEmployer_S");
     while($row = $res->fetch(PDO::FETCH_ASSOC)) {
         echo $row['company_name'] . " | " . $row['overall_rating'];
     }
--- Website/public_html/review_employer.php
+++ Website/public_html/review_employer.php
@@ -2,10 +2,34 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <title>Employer Rankings</title>
+    <title>FakeName.com - Review</title>
 </head>
 <body>
-<h1>Employer Ratings</h1>
+<nav>
+    <ul>
+        <li><a href="index.html">Home</a></li>
+        <li><a href="employer_rankings.php">Employer Rankings</a></li>
+        <li><a href="review_employer.php">Review an Employer</a></li>
+        <li>SignIn / Register</li>
+    </ul>
+</nav>
+<h1>Rate an employer</h1>
+<h3>Form for posting a review for an employer:</h3>
+<p>Search: bar for selecting an employer to review</p>
+<p>Job Title</p>
+<p>Employed From: - Employed To:</p>
+<p>Rating CEO: Approve, Not approve - No Opinion</p>
+<p>Rate: Compensation & Benefits</p>
+<p>Rate: Cultural Values</p>
+<p>Rate: Diversity & Inclusion</p>
+<p>Rate: Senior leadership</p>
+<p>Rate: Work-Life balance</p>
+<p>Rate: Overall Rating</p>
+<p>Recommend to a friend? (Y-N)</p>
+<p>Advice (Text box)</p>
+<p>Pros and Cons (Text box)</p>
+<p>Summary (Text Box)</p>
+
 <?php
 try {
 //    $open_review_s_db = new PDO("sqlite:open_review_s_sqlite.db");
