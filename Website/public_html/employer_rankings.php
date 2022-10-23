656399e128bda39917cbeff76dbf82942b45d80f
Ann Ngo <113649307+bum1234vippro@users.noreply.github.com>
2022-10-24 12:09:51 +1300

    Update employer_rankings.php

--- Website/public_html/employer_rankings.php
+++ Website/public_html/employer_rankings.php
@@ -23,7 +23,7 @@ require_once 'database.php';
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet"
           integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
     <!--                TITLE              -->
-    <title>Best Job Review Tool - OpenReviewPlus.com</title>
+    <title>Best Job Review Tool - OpenReview.com</title>
 
     <script src="js/script.js"></script>
     <link rel="stylesheet" href="css/style.css" />
